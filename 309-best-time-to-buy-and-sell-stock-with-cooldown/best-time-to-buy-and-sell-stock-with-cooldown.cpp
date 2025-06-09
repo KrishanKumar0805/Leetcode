6582d718d141592f1e693c28e48e44e696a21031
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-06-09 13:23:49 +0530

    Time: 2 ms (40.84%) | Memory: 16.9 MB (8.33%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
+    int solve(vector<int>& prices, vector<vector<int>>& dp,int ind,int state){
+        if(ind == prices.size()) return 0 ;
+        if(dp[ind][state] != -1e4) return dp[ind][state] ;
+        int profit = -1e4 ;
+        if(state == 0) profit = max({profit,solve(prices,dp,ind+1,0),
+        -prices[ind]+solve(prices,dp,ind+1,1)});
+        if(state == 1) profit = max({solve(prices,dp,ind+1,1),
+            profit,prices[ind]+solve(prices,dp,ind+1,2)});
+        if(state == 2) profit = max(profit,solve(prices,dp,ind+1,0)) ;
+        return dp[ind][state] = profit ;
+    }
     int maxProfit(vector<int>& prices) { int n = prices.size() ;
-        vector<int> prev(3,-1e5),curr(3,-1e5);
-        prev[0] = -prices[0] ; prev[1]= 0  ; prev[2] = 0 ;
-        for(int i=1;i<n;i++){
-            curr[0] = max(prev[0],prev[2]-prices[i]) ;
-            curr[1] = max(prev[0]+prices[i],curr[1]) ;
-            curr[2] = max(curr[2],prev[1]) ;
-            prev = curr ;
-            // cout << curr[0] <<curr[1]<< curr[2]<< "  ";
-        } return max({0,curr[1],curr[2]});
+        vector<vector<int>> dp(n,vector<int>(3,-1e4)) ;
+        return max(0,solve(prices,dp,0,0)) ;
     }
 };
