b4afffc11a3e5444d645138cd823e02e6cca7dad
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-06-13 10:00:18 +0530

    Time: 7 ms (83.87%) | Memory: 175.1 MB (61.29%) - LeetSync

--- 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
+++ 3653-maximum-subarray-sum-with-length-divisible-by-k/maximum-subarray-sum-with-length-divisible-by-k.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     long long maxSubarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        vector<long long> prefix(n+1),dp(n);
-        for(int i=0;i<n;i++) prefix[i+1]=nums[i]+prefix[i];
-        long long res=dp[k-1]=prefix[k]-prefix[0];
+        int n = nums.size();
+        vector<long long> prefix(n + 1, 0);
+        for (int i = 0; i < n; i++) {
+            prefix[i + 1] = prefix[i] + nums[i];
+        }
+       
+        vector<long long> dp(k, LLONG_MAX);   dp[0] = 0 ;
+        long long ans = LLONG_MIN;
 
-        for(int i=k;i<n;i++) res=max(res,dp[i]=prefix[i+1]-prefix[i-k+1]+max(0LL,dp[i-k]));
-        return res;
+        for (int i = 0; i < n; i++) {
+            int rem = (i + 1) % k;
+            if (dp[rem] != LLONG_MAX) {
+                ans = max(ans, prefix[i + 1] - dp[rem]);
+                dp[rem] = min(dp[rem], prefix[i + 1]);
+            } else {
+                dp[rem] = prefix[i + 1];
+            }
+        }
+
+        return ans;
     }
 };
