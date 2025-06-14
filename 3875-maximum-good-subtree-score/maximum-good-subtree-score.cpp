77cd34be73b14ca21f94a549254267b6e9234d1b
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-06-15 00:48:07 +0530

    Time: 522 ms (42.05%) | Memory: 356 MB (35.06%) - LeetSync

--- 3875-maximum-good-subtree-score/maximum-good-subtree-score.cpp
+++ 3875-maximum-good-subtree-score/maximum-good-subtree-score.cpp
@@ -7,26 +7,19 @@ public:
 
     vector<long long> solve(int u, int par, vector<vector<int>>& adj, vector<int>& vals) {
         vector<long long> dp(M, NEG);
-        dp[0] = 0;
-
-        int val = vals[u];
-        int msk_u = 0;
-        bool valid = true;
+        dp[0] = 0; int val = vals[u];int msk_u = 0;bool valid = true;
         vector<bool> seen(10, false);
         while (val) {
             int d = val % 10;
             if (seen[d]) {
                 valid = false;
-                break;
-            }
+                break;}
             seen[d] = true;
             msk_u |= (1 << d);
             val /= 10;
         }
-        if (valid) {
-            dp[msk_u] = max(dp[msk_u], 1LL * vals[u]);
-        }
-        for (int v : adj[u]) {
+        if (valid)  dp[msk_u] = max(dp[msk_u], 1LL * vals[u]);
+          for (int v : adj[u]) {
             if (v == par) continue;
             vector<long long> child_dp = solve(v, u, adj, vals);
             vector<long long> new_dp(M, NEG);
@@ -37,14 +30,11 @@ public:
                     if (mask1 & mask2) continue;
                     new_dp[mask1 | mask2] = max(new_dp[mask1 | mask2], dp[mask1] + child_dp[mask2]);
                 }
-            }
-            dp = new_dp;
+            }dp = new_dp;
         }
         long long res = 0;
         for (int i = 0; i < M; ++i) 
             res = max(res, dp[i]);
-       
-
         ans = (res + ans) % MOD;
         return dp;
     }
