8ae8185ba8b8cc0445ecbc73ef0442e058176736
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-06-15 01:08:38 +0530

    Time: 562 ms (37.38%) | Memory: 356.1 MB (35.01%) - LeetSync

--- 3875-maximum-good-subtree-score/maximum-good-subtree-score.cpp
+++ 3875-maximum-good-subtree-score/maximum-good-subtree-score.cpp
@@ -18,17 +18,17 @@ public:
             msk_u |= (1 << d);
             val /= 10;
         }
-        if (valid)  dp[msk_u] =  vals[u];
+        if (valid)  dp[msk_u] = vals[u];
           for (int v : adj[u]) {
             if (v == par) continue;
             vector<long long> child_dp = solve(v, u, adj, vals);
             vector<long long> new_dp(M, NEG);
-            for (int mask1 = 0; mask1 < M; ++mask1) {
-                if (dp[mask1] == NEG) continue;
-                for (int mask2 = 0; mask2 < M; ++mask2) {
-                    if (child_dp[mask2] == NEG) continue;
-                    if (mask1 & mask2) continue;
-                    new_dp[mask1 | mask2] = max(new_dp[mask1 | mask2], dp[mask1] + child_dp[mask2]);
+            for (int i = 0; i < M; i++) {
+                if (dp[i] == NEG) continue;
+                for (int j = 0;j < M;j++) {
+                    if (child_dp[j] == NEG) continue;
+                    if (i & j) continue;
+                    new_dp[i|j] = max(new_dp[i|j], dp[i] + child_dp[j]);
                 }
             }dp = new_dp;
         }
