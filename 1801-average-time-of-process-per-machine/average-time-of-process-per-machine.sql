a82c3fd9e19168cd2367e5f129611502ec170ba2
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-06-17 23:46:47 +0530

    Time: 307 ms (26.97%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+select a1.machine_id,
+round(avg(a1.timestamp-a2.timestamp),3) as 
+processing_time
+from Activity a1 cross join Activity a2
+where a1.machine_id=a2.machine_id and
+a1.activity_type='end' and a2.activity_type='start'
+group by a1.machine_id
