d66d1fcaa616a4d8cfbbf4cda7a5aeee8cef6c59
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-06-17 23:50:49 +0530

    Time: 243 ms (93.60%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -2,7 +2,7 @@
 select a1.machine_id,
 round(avg(a1.timestamp-a2.timestamp),3) as 
 processing_time
-from Activity a1 cross join Activity a2
-where a1.machine_id=a2.machine_id and
+from Activity a1 inner join Activity a2
+on  a1.machine_id=a2.machine_id where 
 a1.activity_type='end' and a2.activity_type='start'
 group by a1.machine_id
