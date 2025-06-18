08171f00b774502b9ea05d98ce2a57290532c67f
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-06-18 19:21:19 +0530

    Time: 743 ms (19.13%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,5 @@
 /* Write your T-SQL query statement below */
-select e1.name from Employee e1  join Employee e2 
-on e1.id = e2.managerId
-group by e1.id,e1.name having count(e1.id)>= 5
+select e.name  from Employee e  where (
+    select count(*) from Employee ee where ee.managerId = e.id
+     
+) >= 5
