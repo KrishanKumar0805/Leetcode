d48324c9e85b485d60cd9d3f641aab26bb09b691
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-06-18 19:09:29 +0530

    Time: 371 ms (99.37%) | Memory: 0B (100.00%) - LeetSync

--- 1415-students-and-examinations/students-and-examinations.sql
+++ 1415-students-and-examinations/students-and-examinations.sql
@@ -1,7 +1,9 @@
 /* Write your T-SQL query statement below */
-select st.student_id, st.student_name, sb.subject_name,
-count(e.subject_name) as attended_exams from 
-Students st cross join Subjects sb left join Examinations e 
-on e.student_id = st.student_id and e.subject_name = sb.subject_name
-group by st.student_id,st.student_name,sb.subject_name
-order by st.student_id,sb.subject_name
+select s.student_id,s.student_name,sb.subject_name,(
+select count(*) from Examinations e where 
+s.student_id = e.student_id and sb.subject_name = e.subject_name 
+
+) as attended_exams 
+from Students s   cross join Subjects sb  
+group by s.student_id,s.student_name , sb.subject_name 
+order by s.student_id, sb.subject_name 
