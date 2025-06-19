f4842d005aa043b32290a76c5a5088c3bab302cc
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-06-19 06:32:01 +0530

    Time: 4 ms (33.49%) | Memory: 99.2 MB (11.87%) - LeetSync

--- 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
+++ 3864-count-the-number-of-computer-unlocking-permutations/count-the-number-of-computer-unlocking-permutations.cpp
@@ -2,13 +2,13 @@
 class Solution {
 public: 
      vector<ll> fact; const int mod = 1e9 + 7 ;
-    void solve(){
-        for(int i=2;i<=1e5;i++){
+    void solve(int n ){
+        for(int i=2;i<n;i++){
             fact[i] = (1LL*i*fact[i-1]) % mod ;
         }
     }
     int countPermutations(vector<int>& c) { int n = c.size() ;
-        fact = vector<ll>(1e5+1,1) ; solve() ; 
+        fact = vector<ll>(n,1) ; solve(n) ; 
         if(*min_element(c.begin()+1,c.end()) <= c[0]) return 0 ;
         return fact[n-1]; 
     }
