c5617d5f46a5916b883269d87b5e3848f9f87aa0
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-08-27 05:29:05 +0530

    Time: 230 ms (11.00%) | Memory: 336.2 MB (5.91%) - LeetSync

---
+++ 3979-partition-array-into-k-distinct-groups/partition-array-into-k-distinct-groups.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool partitionArray(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp ;
+        for(int i:nums) mpp[i]++ ;
+        int n = nums.size() ;
+        if(n%k != 0) return false ;
+        for(int i:nums){if(mpp[i] > n/k) return false ;}
+        return true ;
+    }
+};
