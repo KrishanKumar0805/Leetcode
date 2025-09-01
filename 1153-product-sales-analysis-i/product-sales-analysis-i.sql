2f3bee55278c1b1a5e130d1498a596330090067c
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-09-01 11:03:32 +0530

    Time: 1149 ms (87.41%) | Memory: 0B (100.00%) - LeetSync

--- 1153-product-sales-analysis-i/product-sales-analysis-i.sql
+++ 1153-product-sales-analysis-i/product-sales-analysis-i.sql
@@ -1,3 +1,4 @@
-/* Write your T-SQL query statement below */
-select p.product_name,s.year,s.price from Sales s 
-left join Product p on   p.product_id = s.product_id 
+# Write your MySQL query statement below
+select p.product_name,s.year,s.price
+from Sales s left join Product p 
+on p.product_id = s.product_id
