9f39bb81981e7f0df3e67ce1901f2551d765b851
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-09-05 17:29:15 +0530

    Time: 826 ms (98.43%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,3 +1,4 @@
-/* Write your T-SQL query statement below */
-select e.name,b.Bonus from Employee e left join
-Bonus b on e.empId = b.empId where b.bonus < 1000 or b.bonus is null 
+# Write your MySQL query statement below
+select e.name,b.bonus from Employee e left join Bonus b
+on e.empId = b.empId
+where b.bonus < 1000 or b.bonus is null
