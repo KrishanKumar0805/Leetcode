67ea2c76f5870cc8e6d038dc630d5dc3cf9dd127
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-09-30 10:04:53 +0530

    Time: 17 ms (42.30%) | Memory: 184.4 MB (63.81%) - LeetSync

--- 4015-split-array-with-minimum-difference/split-array-with-minimum-difference.cpp
+++ 4015-split-array-with-minimum-difference/split-array-with-minimum-difference.cpp
@@ -7,7 +7,7 @@ public:
         for(int i=0;i<n;i++) pref[i+1] = 0LL + pref[i] + nums[i] ;
         for(int i=1;i<n-1;i++){ 
         if(nums[i] <= nums[i+1] && nums[i] <= nums[i-1]) return -1 ;}
-        ll ans = LLONG_MAX ;
+        ll ans = 1e10 ;
         for(int i=1;i<n-1;i++){
              if(nums[i] == nums[i+1]) ans = abs(sum-2*pref[i+1]);
              else if (nums[i] == nums[i-1]) ans = abs(sum-2*pref[i]);
@@ -16,9 +16,10 @@ public:
                 ans=min(abs(2*pref[i+1]-sum),abs(2*pref[i]-sum)); 
             }   
         }
-        if(ans == LLONG_MAX){
+        if(ans == 1e10){
             if(nums[0] >= nums[1]) ans = abs(sum-2*nums[0]);
             else ans = abs(sum-2*nums[n-1]);
-        } return ans;
+        }
+         return ans;
     }
 };
