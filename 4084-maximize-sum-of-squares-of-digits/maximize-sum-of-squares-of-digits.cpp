3b1163e2a42433bb7c224460776a4c98674a38cc
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-10-26 00:58:08 +0530

    Time: 6 ms (76.19%) | Memory: 64.2 MB (9.52%) - LeetSync

--- 4084-maximize-sum-of-squares-of-digits/maximize-sum-of-squares-of-digits.cpp
+++ 4084-maximize-sum-of-squares-of-digits/maximize-sum-of-squares-of-digits.cpp
@@ -4,7 +4,7 @@ public:
         int div = sum / 9 ; string s2 = "" ;
         int rem = sum % 9 ;  if( 9 * num < sum) return "" ;
         string s1 (div,'9') ;
-        if(rem>0 ) s2 = to_string(rem); 
+        rem > 0 ? s2 = to_string(rem) : s2  = "" ; 
         int l = num - div - (rem >0 ? 1 : 0) ;
         string s3(l,'0') ;
         return (s1 + s2 + s3) ;
