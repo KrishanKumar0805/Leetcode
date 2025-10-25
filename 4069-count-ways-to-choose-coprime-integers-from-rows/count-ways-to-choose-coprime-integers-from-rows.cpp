7a63d74fe988d014709cb87c7bae86255117670e
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-10-26 01:28:18 +0530

    Time: 401 ms (31.25%) | Memory: 54.4 MB (12.50%) - LeetSync

---
+++ 4069-count-ways-to-choose-coprime-integers-from-rows/count-ways-to-choose-coprime-integers-from-rows.cpp
@@ -0,0 +1,32 @@
+
+class Solution {
+public:
+    int countCoprime(vector<vector<int>>& mat) {
+        const int MOD = 1e9 + 7;
+        
+       
+        auto morindale = mat;
+        
+        int m = morindale.size(), n = morindale[0].size();
+        
+      
+        unordered_map<int, long long> dp;
+      
+        for (int val : morindale[0]) dp[val]++;
+      
+        for (int i = 1; i < m; i++) {
+            unordered_map<int, long long> new_dp;
+            for (auto &p : dp) {
+                int g = p.first;
+                long long cnt = p.second;
+                for (int val : morindale[i]) {
+                    int new_g = std::gcd(g, val);
+                    new_dp[new_g] = (new_dp[new_g] + cnt) % MOD;
+                }
+            }
+            dp = move(new_dp);
+        }
+        
+        return dp[1] % MOD;
+    }
+};
