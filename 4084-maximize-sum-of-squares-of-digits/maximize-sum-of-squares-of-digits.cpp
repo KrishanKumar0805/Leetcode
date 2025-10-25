47b0fef30a05a93c749149dd88dc1fa0382a0cd6
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-10-26 00:57:11 +0530

    Time: 8 ms (52.38%) | Memory: 64.2 MB (9.52%) - LeetSync

--- 4084-maximize-sum-of-squares-of-digits/maximize-sum-of-squares-of-digits.cpp
+++ 4084-maximize-sum-of-squares-of-digits/maximize-sum-of-squares-of-digits.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     string maxSumOfSquares(int num, int sum) {
-        string ans(num,'0'); 
-        if (  num * 9 < sum) return "" ;
-        for(int i=0;i<num;i++){
-            if(sum > 9) {ans[i] = '9' ;
-            sum -= 9 ; }
-            else {
-                char ch  = '0' + sum ;
-                ans[i] = ch ;
-                break ;
-            }
-        
-        } return ans ;
+        int div = sum / 9 ; string s2 = "" ;
+        int rem = sum % 9 ;  if( 9 * num < sum) return "" ;
+        string s1 (div,'9') ;
+        if(rem>0 ) s2 = to_string(rem); 
+        int l = num - div - (rem >0 ? 1 : 0) ;
+        string s3(l,'0') ;
+        return (s1 + s2 + s3) ;
     }
 };
