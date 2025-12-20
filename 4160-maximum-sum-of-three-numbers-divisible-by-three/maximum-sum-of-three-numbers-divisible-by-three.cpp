13907540b37920c56d3f72feb1b4ee1895e292dc
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2025-12-21 00:48:07 +0530

    Time: 446 ms (5.02%) | Memory: 372.3 MB (5.00%) - LeetSync

---
+++ 4160-maximum-sum-of-three-numbers-divisible-by-three/maximum-sum-of-three-numbers-divisible-by-three.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maximumSum(vector<int>& nums) {
+        multiset<int> zero,one,two;
+        int n= nums.size() ;
+        for(int i=0;i<n;i++){
+            if(nums[i]%3 == 0) zero.insert(nums[i]) ;
+            else if(nums[i]%3 == 1) one.insert(nums[i]) ;
+            else two.insert(nums[i]) ;
+
+            if(zero.size()>3) zero.erase(zero.begin()) ;
+            if(one.size()>3) one.erase(one.begin()) ;
+            if(two.size()>3) two.erase(two.begin()) ;
+        }
+        int p  = zero.size(), q = one.size(),r= two.size() ;
+        if((p< 3 )&& (q < 3 ) && (r<3) && (p==0 || q==0 || r==0)) return 0 ;
+        int ans = 0;
+        if(q == 3) ans = max(ans,accumulate(one.begin(),one.end(),0)) ;
+        if(p == 3) ans = max(ans,accumulate(zero.begin(),zero.end(),0)) ;
+        if(r == 3) ans = max(ans,accumulate(two.begin(),two.end(),0));
+        if(p!= 0 && q!= 0 &&r!=0)
+        ans = max(ans,(*prev(zero.end())+*prev(two.end())+*prev(one.end()) ));
+        return ans;
+    }
+};
