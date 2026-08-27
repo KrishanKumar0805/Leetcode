266c17d4e5b322f2a6f64f70809f72e6cadf37e5
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2026-08-27 09:21:05 +0530

    Time: 2 ms (52.29%) | Memory: 9.3 MB (65.09%) - LeetSync

--- 4381-check-ascii-palindromic/check-ascii-palindromic.cpp
+++ 4381-check-ascii-palindromic/check-ascii-palindromic.cpp
@@ -3,8 +3,7 @@ public:
     bool isPalindromic(string s) {string ans = "";
         for(char ch : s ) ans += bitset<8>(ch).to_string();
         int n = ans.size();
-        for(int i=0;i<n/2;i++){
-            if(ans[i] != ans[n-i-1]) return false ;
-        } return true ;
+        for(int i=0;i<n/2;i++)  if(ans[i] != ans[n-i-1]) return false ;
+         return true ;
     }
 };
