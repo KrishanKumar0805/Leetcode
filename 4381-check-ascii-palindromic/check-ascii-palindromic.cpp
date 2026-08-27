9168ff11865c759df22b3baf49ce3eeb10027493
KRISHAN KUMAR <136729419+KrishanKumar0805@users.noreply.github.com>
2026-08-27 09:20:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (65.09%) - LeetSync

--- 4381-check-ascii-palindromic/check-ascii-palindromic.cpp
+++ 4381-check-ascii-palindromic/check-ascii-palindromic.cpp
@@ -1,12 +1,7 @@
 class Solution {
 public:
-    bool isPalindromic(string s) {
-        string ans = "";
-        for(char ch : s ){
-            bitset<8> bin(ch);
-            ans += bin.to_string();
-        }
-        cout << ans << endl ;
+    bool isPalindromic(string s) {string ans = "";
+        for(char ch : s ) ans += bitset<8>(ch).to_string();
         int n = ans.size();
         for(int i=0;i<n/2;i++){
             if(ans[i] != ans[n-i-1]) return false ;
